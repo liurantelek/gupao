fdf019209e49904e87f1bbc3062b4822f9297ca3
liuran <454176801@qq.com>
2020-07-06 18:49:45 +0800

    mvc

---
+++ springlrframework/src/main/java/com/lr/framework/webmvc/servlet/LrVierResolver.java
@@ -0,0 +1,23 @@
+package com.lr.framework.webmvc.servlet;
+
+import java.util.Locale;
+
+/**
+ * @version v1.0
+ * @ProjectName: gupao
+ * @ClassName: LrVierResolver
+ * @Description: TODO(一句话描述该类的功能)
+ * @Author: 刘然
+ * @Date: 2020/7/4 10:48
+ */
+public class LrVierResolver {
+
+
+    public LrVierResolver(String templateRoot) {
+
+    }
+
+    public LrView resolvewName(String viewName, Locale locale)throws Exception{
+        return null;
+    }
+}
---
+++ springlrframework/src/main/java/com/lr/framework/webmvc/servlet/LrView.java
@@ -0,0 +1,22 @@
+package com.lr.framework.webmvc.servlet;
+
+import com.sun.istack.internal.Nullable;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+/**
+ * @version v1.0
+ * @ProjectName: gupao
+ * @ClassName: LrView
+ * @Description: TODO(一句话描述该类的功能)
+ * @Author: 刘然
+ * @Date: 2020/7/4 10:52
+ */
+public class LrView {
+
+    public void render(@Nullable Map<String,Object> model, HttpServletRequest req, HttpServletResponse resp) throws Exception{
+
+    }
+}
