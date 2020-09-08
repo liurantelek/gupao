33f9914510c4efa7be5d03f9ee849f75b66bf5e8
liuran <454176801@qq.com>
2020-09-08 18:00:33 +0800

    ss

--- nettydemo/src/main/java/tomcat/http/LRResponse.java
+++ nettydemo/src/main/java/tomcat/http/LRResponse.java
@@ -1,5 +1,6 @@
 package tomcat.http;
 
+import java.io.IOException;
 import java.io.OutputStream;
 
 /**
@@ -7,11 +8,21 @@ import java.io.OutputStream;
  * @create 2020-09-07-22:45
  */
 public class LRResponse {
+    private OutputStream os;
 
     public LRResponse (OutputStream os){
-
+        this.os = os;
     }
 
     public void write(String s) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("HTTP/1.1 200 OK");
+        sb.append("Content-Type: text/html;charset=utf-8");
+        sb.append(s);
+        try {
+            os.write(s.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
